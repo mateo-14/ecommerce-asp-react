061faceca0cf846b482b888491a48c371bd35ef2
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2025-02-07 17:32:44 -0300

    Fix products query key

--- frontend/src/queries/productsQueryOptions.ts
+++ frontend/src/queries/productsQueryOptions.ts
@@ -2,6 +2,6 @@ import { getProducts } from '@/services/productsService'
 import { queryOptions } from '@tanstack/react-query'
 
 export const productsQueryOptions = (page: number = 1, pageSize: number = 10) => queryOptions({
-  queryKey: ['categories', { page, pageSize }],
+  queryKey: ['products', { page, pageSize }],
   queryFn: () => getProducts(page, pageSize),
 })
